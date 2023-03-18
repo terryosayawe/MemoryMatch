a1649ea9530522bd4af5d83df8543d9c8a8171fe
Ogbemudia Terry Osayawe <terrymarcy2000@yahoo.com>
2023-03-18 22:51:08 +0100

    Initial Commit

---
+++ MemoryMatch/MemoryMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryMatchApp.swift
+//  MemoryMatch
+//
+//  Created by Ogbemudia Terry Osayawe on 18.03.23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
